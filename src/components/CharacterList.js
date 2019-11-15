2a6e50a523f17c4f29aba47c32e78f5e02fb8fce
toddmurphy <toddmurphy93@gmail.com>
2019-11-15 12:54:06 -0400

    connected to api and was able to get data

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,10 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([])
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
+    axios.get('https://rickandmortyapi.com/api/character/')
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error => {
+        console.log('No character data returned', error)
+      })
+
+
+
+
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
 
